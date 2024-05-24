5abcaa02897aee2579ab3fcef329ccca58902f78
Augusto Pedraza <augusto.pedraza08@gmail.com>
2024-05-24 20:34:01 -0300

    Repair tests [⚠️]

--- test/reactor_ap_web/live/product_live_test.exs
+++ test/reactor_ap_web/live/product_live_test.exs
@@ -5,7 +5,12 @@ defmodule ReactorApWeb.ProductLiveTest do
   import ReactorAp.CatalogFixtures
 
   @create_attrs %{description: "some description", name: "some name", sku: 42, unit_price: 120.5}
-  @update_attrs %{description: "some updated description", name: "some updated name", sku: 43, unit_price: 456.7}
+  @update_attrs %{
+    description: "some updated description",
+    name: "some updated name",
+    sku: 43,
+    unit_price: 456.7
+  }
   @invalid_attrs %{description: nil, name: nil, sku: nil, unit_price: nil}
 
   defp create_product(_) do
@@ -14,7 +19,7 @@ defmodule ReactorApWeb.ProductLiveTest do
   end
 
   describe "Index" do
-    setup [:create_product]
+    setup [:create_product, :register_and_log_in_user]
 
     test "lists all products", %{conn: conn, product: product} do
       {:ok, _index_live, html} = live(conn, ~p"/products")
@@ -78,7 +83,7 @@ defmodule ReactorApWeb.ProductLiveTest do
   end
 
   describe "Show" do
-    setup [:create_product]
+    setup [:create_product, :register_and_log_in_user]
 
     test "displays product", %{conn: conn, product: product} do
       {:ok, _show_live, html} = live(conn, ~p"/products/#{product}")
