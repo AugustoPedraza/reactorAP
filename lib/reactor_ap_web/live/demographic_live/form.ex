a795a7ea4b7a4e755082e3e753e58821fd278614
Augusto Pedraza <augusto.pedraza08@gmail.com>
2024-06-12 12:38:38 -0300

    Add basic form to create Demographic

---
+++ lib/reactor_ap_web/live/demographic_live/form.ex
@@ -0,0 +1,38 @@
+defmodule ReactorApWeb.DemographicLive.Form do
+  use ReactorApWeb, :live_component
+
+  alias ReactorAp.Survey
+  alias ReactorAp.Survey.Demographic
+
+  def update(%{current_user: current_user} = assigns, socket) do
+    changeset =
+      %Demographic{user_id: current_user.id}
+      |> Survey.change_demographic()
+
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> assign_form(changeset)}
+  end
+
+  def handle_event("save", %{"demographic" => demographic_params}, socket) do
+    save_demographic(socket, demographic_params)
+  end
+
+  defp assign_form(socket, %Ecto.Changeset{} = changeset) do
+    assign(socket, :form, to_form(changeset))
+  end
+
+  defp save_demographic(socket, params) do
+    case Survey.create_demographic(params) do
+      {:ok, demographic} ->
+        notify_parent({:created_demographic, demographic})
+        socket
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign_form(socket, changeset)}
+    end
+  end
+
+  defp notify_parent(msg), do: send(self(), {__MODULE__, msg})
+end
--- lib/reactor_ap_web/live/demographic_live/show.ex
+++ lib/reactor_ap_web/live/demographic_live/show.ex
@@ -3,7 +3,6 @@ defmodule ReactorApWeb.DemographicLive.Show do
   use Phoenix.HTML
 
   alias ReactorAp.Survey.Demographic
-  alias ReactorApWeb.CoreComponents
 
   attr :demographic, Demographic, required: true
 
@@ -11,14 +10,6 @@ defmodule ReactorApWeb.DemographicLive.Show do
     ~H"""
     <div>
       <h2 class="font-medium text-2xl">Demographics <%= raw("&#x2713;") %></h2>
-      <CoreComponents.table rows={[@demographic]}>
-        <:col :let={demographic} label="Gender">
-          <%= demographic.gender %>
-        </:col>
-        <:col :let={demographic} label="Year of Birth">
-          <%= demographic.year_of_birth %>
-        </:col>
-      </CoreComponents.table>
       <ul>
         <li>Gender: <%= @demographic.gender %></li>
         <li>Year of birth: <%= @demographic.year_of_birth %></li>
